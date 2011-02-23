2bafc6c5965a10c2efd8e6c77fbe0f8bcc2bc7fb
Sebastian Klepper <sebastian.klepper@gmail.com>
2011-02-23 22:33:37 +0100

    Now using Lemonade to create image sprites. Also, better image spacing.

--- config.rb
+++ config.rb
@@ -1,7 +1,9 @@
+require "lemonade"
+
 http_path = "/launchpad/"
 sass_dir = "src"
 css_dir = "stylesheets"
 images_dir = "images"
 javascripts_dir = "scripts"
-relative_assets = false
+relative_assets = true
 output_style = :compact
